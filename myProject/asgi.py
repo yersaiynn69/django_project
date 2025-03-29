f2305e10953e924c0e839d4cebf21aa4ca07151d
yersaiynn69 <100183617+yersaiynn69@users.noreply.github.com>
2025-03-29 13:08:17 +0500

    asgi.py

--- myProject/asgi.py
+++ myProject/asgi.py
@@ -1,10 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 
-# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myProject.settings')
 
-# Create the ASGI application
 application = get_asgi_application()
