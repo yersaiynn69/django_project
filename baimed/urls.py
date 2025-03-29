c2a4ba44eea95b80bf7abd49560d9be712159a5a
yersaiynn69 <100183617+yersaiynn69@users.noreply.github.com>
2025-03-29 11:59:07 +0500

    urls.py

--- baimed/urls.py
+++ baimed/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('home', index, name='home'),
+    path('', index, name='home'),  # Главная
     path('product/', product, name='product'),
     path('contact/', contact_formview, name='contact'),
-    path('setting', setting, name='setting'),
-    path('householdDoctor', householdDoctor, name='householdDoctor'),
-
-    path('post/<slug:post_slug>/,', show_post, name='post'),
+    path('setting/', setting, name='setting'),
+    path('householdDoctor/', householdDoctor, name='householdDoctor'),
+    path('post/<slug:post_slug>/', show_post, name='post'),
 ]
