65e95c20339ac881a397f1e295efc9d7f3ffc12c
yersaiynn69 <100183617+yersaiynn69@users.noreply.github.com>
2025-03-29 11:56:07 +0500

    urls.py

--- myProject/urls.py
+++ myProject/urls.py
@@ -1,22 +1,32 @@
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-
 from django.urls import path, include, re_path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from django.conf import settings
 from django.views.static import serve
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # App-specific routes
     path('', include('baimed.urls')),
-    path('', include('appointments.urls')),
-    path('', include('chat.urls')),
-    path('', include('authentication.urls')),
-    path('',include('about_us.urls')),
+    path('appointments/', include('appointments.urls')),
+    path('chat/', include('chat.urls')),
+    path('auth/', include('authentication.urls')),
+    path('about_us/', include('about_us.urls')),
+
+    # Captcha and Social Auth
     path('captcha/', include('captcha.urls')),
-    re_path('', include('social_django.urls', namespace='social')),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('social-auth/', include('social_django.urls', namespace='social')),
+
+    # Static and media for development
+]
+
+# Serve media and static only in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
