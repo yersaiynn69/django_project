24abee0e9b25baeb3581d5097ff56a0a86ba2902
yersaiynn69 <100183617+yersaiynn69@users.noreply.github.com>
2025-03-29 12:48:06 +0500

    settings.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -90,7 +90,6 @@ SOCIAL_AUTH_JSONFIELD_ENABLED = True
 
 MIDDLEWARE = [
     'myProject.middleware.KazakhstanOnlyMiddleware',
- #   'myProject.middleware.SimpleFirewallMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
