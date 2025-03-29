edc547027dac3192c6e68cbba9ca6751da1a1728
yersaiynn69 <100183617+yersaiynn69@users.noreply.github.com>
2025-03-29 11:59:43 +0500

    urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,9 @@
-from django.contrib.auth import views
 from django.urls import path
-from authentication.views import *
+from . import views
 
 urlpatterns = [
-    path('login/', LoginUser.as_view(), name='login'),
-    path('register/', RegisterUser.as_view(), name='register'),
-    path('logout/', logout_user, name='logout'),
-    path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('setting/', views.setting_view, name='setting'),
 ]
