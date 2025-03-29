05b185b3c811c082b49977a994b0fe67fd45ec49
yersaiynn69 <100183617+yersaiynn69@users.noreply.github.com>
2025-03-29 12:00:11 +0500

    urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,7 @@
-from baimed.views import index
-from chat.views import room
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('chat/<str:room_name>/', room, name='room'),
+    path('', views.chat_home, name='chat_home'),
+    path('<str:room_name>/', views.chat_room, name='chat'),
 ]
